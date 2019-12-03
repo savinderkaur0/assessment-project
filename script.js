3ac7cb69f26eb70f2a18a32a3a33c611e151d274
savinderkaur0 <savinderkaur00000@gmail.com>
2019-12-03 20:38:51 +0530

    comit

--- script.js
+++ script.js
@@ -12,19 +12,19 @@ if (this.readyState == 4 && this.status == 200)
   var get_tag=document.getElementById("image1")
   document.getElementById("id1").innerHTML = res;
   get_tag.setAttribute('src',url)
-  get_tag.setAttribute("height","200px")
+  get_tag.setAttribute("height","300px")
   var url1=obj.meals[3].strMealThumb
   var res=obj.meals[3].strMeal
   var get_tag1=document.getElementById("image2")
   document.getElementById("id2").innerHTML = res;
   get_tag1.setAttribute('src',url1)
-    get_tag1.setAttribute("height","200px")
+    get_tag1.setAttribute("height","300px")
     var url2=obj.meals[4].strMealThumb
     var res=obj.meals[4].strMeal
     var get_tag2=document.getElementById("image3")
     document.getElementById("id3").innerHTML = res;
     get_tag2.setAttribute('src',url2)
-      get_tag2.setAttribute("height","200px")
+      get_tag2.setAttribute("height","300px")
 }
 }
 xhttp.open("GET", "https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian", true);
